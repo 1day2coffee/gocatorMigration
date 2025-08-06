d863f14ef31e9892ef01e45edf07793f9d03fd12
1day2coffee <123rbdud@gmail.com>
2025-08-06 14:20:53 +0900

    chore: 로고추가, 타이틀수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,24 +12,25 @@ export function Header() {
                     <div className="flex h-16 items-center justify-between">
                         <div className="flex items-center space-x-4">
                             <div className="flex items-center space-x-2">
-                                <Camera className="h-8 w-8 text-blue-400" />
-                                <h1 className="text-xl font-bold text-white">Gocator Vision System</h1>
+                                <h1 className="text-xl font-bold text-white">SoltiTM - 3D Vision Quality Checking System</h1>
                             </div>
-                            <Badge variant="secondary" className="bg-green-500/10 text-green-400 border-green-500/20 hover:bg-green-500/10">
+                            {/* <Camera className="h-8 w-8 text-blue-400" /> */}
+                            {/* <Badge variant="secondary" className="bg-green-500/10 text-green-400 border-green-500/20 hover:bg-green-500/10">
                                 <Wifi className="w-3 h-3 mr-1" />
                                 Connected
-                            </Badge>
+                                </Badge> */}
                         </div>
 
                         <div className="flex items-center space-x-4">
-                            <Button variant="outline" size="sm" className="border-slate-600 text-slate-300 hover:bg-slate-700">
+                                <img className="h-11 w-24" src="logo.png"/>
+                            {/* <Button variant="outline" size="sm" className="border-slate-600 text-slate-300 hover:bg-slate-700">
                                 <Settings className="w-4 h-4 mr-2" />
                                 Settings
                             </Button>
                             <Button variant="outline" size="sm" className="border-slate-600 text-slate-300 hover:bg-slate-700">
                                 <FileText className="w-4 h-4 mr-2" />
                                 Logs
-                            </Button>
+                            </Button> */}
                         </div>
                     </div>
                 </div>
